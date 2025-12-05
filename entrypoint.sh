44fcb426cc59a8d85d797b0e8fa5be5dda2790cd
xtof <christophe.alexandre@keplertech.io>
2025-12-05 12:35:13 +0100

    update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ DESIGN_CONFIG="$2"
 #LIBRARY="$4"
 
 export YOSYS_EXE="/yosys/bin/yosys"
-export KEPLER_FORMAL_EXE="//"
+export KEPLER_FORMAL_EXE="/kepler-formal/bin/kepler_formal"
 export DESIGN_HOME="$ROOT/$DESIGN_HOME"
 export DESIGN_CONFIG="$ROOT/$DESIGN_CONFIG"
 
@@ -15,6 +15,7 @@ echo "::group::Naja-Direct-Yosys"
 echo "Naja Direct Yosys mode selected"
 echo "Yosys executable: $YOSYS_EXE"
 echo "Yosys version: $($YOSYS_EXE -V)"
+echo "Kepler Formal executable: $KEPLER_FORMAL_EXE"
 echo "Design config: $DESIGN_CONFIG"
 mkdir naja-run
 cd naja-run
