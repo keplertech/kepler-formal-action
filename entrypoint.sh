a2b6ea33482149696e00676d493ec16e09ffafb5
xtof <christophe.alexandre@keplertech.io>
2025-12-05 08:33:12 +0100

    cleaning action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,51 +2,31 @@
 set -e
 
 ROOT="/github/workspace"
-MODE="$1"
-DESIGN_HOME="$2"
-DESIGN_CONFIG="$3"
+DESIGN_HOME="$1"
+DESIGN_CONFIG="$2"
 #LIBRARY="$4"
 
 export YOSYS_EXE="/yosys/bin/yosys"
 export DESIGN_HOME="$ROOT/$DESIGN_HOME"
 export DESIGN_CONFIG="$ROOT/$DESIGN_CONFIG"
 
-if [ "$MODE" == "orfs" ]; then
-    echo "::group::Naja-ORFS"
-    echo "Naja ORFS mode selected"
-    #LIBERTY_FILE="/najaeda-or/flow/objects/nangate45/bp/base/lib/*.lib"
-    #VERILOG_FILE="/najaeda-or/flow/results/nangate45/bp/base/1_synth.v"
-    cd /najaeda-or/flow && make synth
-    #echo "Liberty file: $LIBERTY_FILE"
-    #echo "$(ls /najaeda-or/flow/objects/nangate45/bp/base/lib)"
-    echo "Verilog file: $VERILOG_FILE"
-    echo "::endgroup::"
+echo "::group::Naja-Direct-Yosys"
+echo "Naja Direct Yosys mode selected"
+echo "Yosys executable: $YOSYS_EXE"
+echo "Yosys version: $($YOSYS_EXE -V)"
+echo "Design config: $DESIGN_CONFIG"
+mkdir naja-run
+cd naja-run
+#export SYNTH_ROOT="$ROOT/najaeda-or/flow"
+#export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
+$YOSYS_EXE -c "$DESIGN_CONFIG"
+VERILOG_FILE="naja_netlist.v"
+echo "Verilog file: $VERILOG_FILE"
+echo "::endgroup::"
 
-    echo "::group::Launching-Najaeda"
-    python3 /najaeda_scripts/count_leaves.py --primitives_mode="liberty" --liberty "$LIBERTY_FILE" --verilog "$VERILOG_FILE" 
-    cp design.stats /github/workspace/design.stats
-    echo "::endgroup::"
+echo "::group::Launching-Najaeda"
+python3 /najaeda_scripts/count_leaves.py --primitives_mode="xilinx" --verilog "$VERILOG_FILE"
+cp design.stats /github/workspace/design.stats
+echo "::endgroup::"
 
-    echo "Done!"
-else
-    echo "::group::Naja-Direct-Yosys"
-    echo "Naja Direct Yosys mode selected"
-    echo "Yosys executable: $YOSYS_EXE"
-    echo "Yosys version: $($YOSYS_EXE -V)"
-    echo "Design config: $DESIGN_CONFIG"
-    mkdir naja-run
-    cd naja-run
-    #export SYNTH_ROOT="$ROOT/najaeda-or/flow"
-    #export SYNTH_ROOT="$ROOT/$SYNTH_ROOT"
-    $YOSYS_EXE -c "$DESIGN_CONFIG"
-    VERILOG_FILE="naja_netlist.v"
-    echo "Verilog file: $VERILOG_FILE"
-    echo "::endgroup::"
-
-    echo "::group::Launching-Najaeda"
-    python3 /najaeda_scripts/count_leaves.py --primitives_mode="xilinx" --verilog "$VERILOG_FILE"
-    cp design.stats /github/workspace/design.stats
-    echo "::endgroup::"
-
-    echo "Done!"
-fi
+echo "Done!"
